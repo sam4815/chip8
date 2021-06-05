0cc8f53f123c1de5b85db38a3f043eeaf9c6ccba
sam <sam.brougham@gmail.com>
2021-06-05 17:02:12 -0400

    Make renderer themeable

--- src/hooks/useRenderer.js
+++ src/hooks/useRenderer.js
@@ -1,25 +1,35 @@
 import { useEffect, useRef } from 'react';
 
-const HEIGHT = 32;
-const WIDTH = 64;
+const CHIP_8_HEIGHT = 32;
+const CHIP_8_WIDTH = 64;
 
 const init2DArray = (n, m) =>
   Array.from({ length: n }).map(() => Array.from({ length: m }).fill(0));
 
-const useRenderer = () => {
+const useRenderer = (theme) => {
   const canvasRef = useRef(null);
-  const display = useRef(init2DArray(WIDTH, HEIGHT));
-  const scale = screen.width / WIDTH;
+  const display = useRef(init2DArray(CHIP_8_WIDTH, CHIP_8_HEIGHT));
+  const scale = useRef(1);
+  const canvasTheme = useRef(theme);
 
   useEffect(() => {
-    const context = canvasRef.current.getContext('2d');
-    context.canvas.height = HEIGHT * scale;
-    context.canvas.width = WIDTH * scale;
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+
+    canvas.style.width = '100%';
+    scale.current = canvas.offsetWidth / CHIP_8_WIDTH;
+
+    context.canvas.width = CHIP_8_WIDTH * scale.current;
+    context.canvas.height = CHIP_8_HEIGHT * scale.current;
   }, [canvasRef]);
 
+  useEffect(() => {
+    canvasTheme.current = theme;
+  }, [theme]);
+
   const setPixel = (x, y, val) => {
-    x = x % WIDTH;
-    y = y % HEIGHT;
+    x = x % CHIP_8_WIDTH;
+    y = y % CHIP_8_HEIGHT;
 
     const collision = display.current[x][y] & val;
     display.current[x][y] ^= val;
@@ -27,17 +37,31 @@ const useRenderer = () => {
   };
 
   const clear = () => {
-    display.current = init2DArray(WIDTH, HEIGHT);
+    display.current = init2DArray(CHIP_8_WIDTH, CHIP_8_HEIGHT);
   };
 
   const draw = () => {
     const context = canvasRef.current.getContext('2d');
-    context.clearRect(0, 0, WIDTH * scale, HEIGHT * scale);
-    context.fillStyle = '#000';
+    context.fillStyle = canvasTheme.current.background;
+    context.fillRect(
+      0,
+      0,
+      CHIP_8_WIDTH * scale.current,
+      CHIP_8_HEIGHT * scale.current
+    );
+
+    context.fillStyle = canvasTheme.current.fill;
 
     display.current.forEach((row, x) =>
       row.forEach(
-        (val, y) => val && context.fillRect(x * scale, y * scale, scale, scale)
+        (val, y) =>
+          val &&
+          context.fillRect(
+            x * scale.current,
+            y * scale.current,
+            Math.ceil(scale.current + 1),
+            Math.ceil(scale.current + 1)
+          )
       )
     );
   };
