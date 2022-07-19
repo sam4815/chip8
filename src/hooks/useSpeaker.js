acf051f6c8180d418e49a7cae296e1786ca244ab
sam <sam.brougham@gmail.com>
2022-07-18 21:49:32 -0400

    Switch sound to simple drum beat

--- src/hooks/useSpeaker.js
+++ src/hooks/useSpeaker.js
@@ -8,29 +8,22 @@ const useSpeaker = () => {
   const playing = useRef(false);
 
   useEffect(() => {
-    const fetchSounds = async () => {
-      const buffers = await Promise.all(
-        Array.from({ length: 3 }).map(async (_, i) => {
-          const request = await fetch(
-            `${process.env.PUBLIC_URL}/mallet${i + 1}.mp3`
-          );
-          const arrayBuffer = await request.arrayBuffer();
-          return audio.current.decodeAudioData(arrayBuffer);
-        })
-      );
+    const fetchSound = async () => {
+      const request = await fetch(`${process.env.PUBLIC_URL}/beat.mp3`);
+      const arrayBuffer = await request.arrayBuffer();
+      const buffer = await audio.current.decodeAudioData(arrayBuffer);
 
-      soundBuffers.current = buffers;
+      soundBuffers.current = buffer;
     };
 
-    fetchSounds();
+    fetchSound();
   }, []);
 
   const play = () => {
     if (playing.current || !audio.current) return;
 
     const source = audio.current.createBufferSource();
-    const buffers = soundBuffers.current;
-    source.buffer = buffers[Math.floor(Math.random() * buffers.length)];
+    source.buffer = soundBuffers.current;
     source.connect(audio.current.destination);
     source.start();
 
